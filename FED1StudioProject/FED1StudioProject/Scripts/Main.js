8e717557ec21c6348c436fcd0793dc85dd104606
BlaineG106 <blaineg3291@gmail.com>
2020-12-14 09:39:57 -0600

    Finished Project

---
+++ FED1StudioProject/FED1StudioProject/Scripts/Main.js
@@ -0,0 +1,14 @@
+﻿jQuery(document).ready(function () {
+    console.log("jQuery is working!");
+
+
+    $(".leadershipli img").on("click", function () {
+        let imgScr = $(this).attr("src");
+        $("#lightbox-content").html('<img src="' + imgScr + '" style="max-width: 300px;" class="img-responsive" />');
+        $("#lightbox-container").fadeIn(500);
+        $("#lightbox-container").on("click", function () {
+            $(this).fadeOut(500);
+        });
+    });
+});
+
---
+++ FED1StudioProject/FED1StudioProject/Scripts/contactForm.js
@@ -0,0 +1,30 @@
+﻿function validateForm(event) {
+    //define variables for both the text boxes and the invalid messages
+    let controls = document.getElementsByClassName('form-control');
+    let validationMessages = document.getElementsByClassName('invalid');
+
+    //Check that they filled out each text box, if not display error message
+    if (controls['Name'].value.length == 0 || controls['Email'].value.length == 0 || controls['Message'].value.length == 0) {
+        event.preventDefault();
+
+        if (controls['Name'].value.length == 0) {
+            validationMessages['rfvName'].textContent = "* Name is Required";
+        }
+        else {
+            validationMessages['rfvName'].textContent = "";
+        }
+        if (controls['Email'].value.length == 0) {
+            validationMessages['rfvEmail'].textContent = "* Email is Required";
+        }
+        else {
+            validationMessages['rfvEmail'].textContent = "";
+        }
+        if (controls['Message'].value.length == 0) {
+            validationMessages['rfvMessage'].textContent = "* Message is Required";
+        }
+        else {
+            validationMessages['rfvMessage'].textContent = "";
+        }
+    }
+
+}
